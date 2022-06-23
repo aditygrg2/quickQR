07777dfc732bbee26c70e803de4657738dcbcbe9
Aditya Garg <aditygrg@Adityas-MacBook-Air.local>
2022-06-23 17:07:34 +0530

    First commit:create the directory and setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const port = 8000;
+
+
+
+
+
+
+
+app.listen(port,function(err){
+    if(err){
+        // console.log("Error",err);
+        //Interpolation
+        console.log(`Error in running the server : ${err}`);
+    }
+    else{
+        console.log(`Server is up and running at port ${port}`);
+    }
+})
