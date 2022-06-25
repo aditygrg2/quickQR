36ca0e031b40b8e330cd86f7a1266858d3649bc4
Aditya Garg <uber6707@gmail.com>
2022-06-25 14:00:04 +0530

    Proper working

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,19 @@
 module.exports.profile = function(req,res){
     return res.end('<h1>User Profile</h1>');
 }
+
+module.exports.in = function(req,res){
+    return res.render('signin');
+}
+
+module.exports.up = function(req,res){
+    return res.render('signup');
+}
+
+module.exports.create = function(req,res){
+
+}
+
+module.exports.createSession = function(req,res){
+    
+}
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile',usersController.profile);
+router.get('/signin',usersController.in);
+router.get('/signup',usersController.up);
 
 module.exports = router;
